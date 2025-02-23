711251c3e2e21461ab3332b4bb11f5d28e05d308
Kax675 <hi@kax.one>
2025-02-23 15:21:02 +0300

    feat: update the ports

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	server: {
-		port: 3030
-	},
 	plugins: [
 		tailwindcss(),
 		sveltekit(),
