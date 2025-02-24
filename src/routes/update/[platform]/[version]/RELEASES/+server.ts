7dc8c35608d9c8c3089d349bd4bb678cf142e478
Kax675 <hi@kax.one>
2025-02-25 00:24:09 +0300

    fix: debugging for the server

--- src/routes/update/[platform]/[version]/RELEASES/+server.ts
+++ src/routes/update/[platform]/[version]/RELEASES/+server.ts
@@ -2,6 +2,7 @@ import { json, text } from '@sveltejs/kit';
 import { loadCache } from '$lib/cache';
 export async function GET() {
 	const cache = await loadCache();
+	console.log(cache)
 
 	return text(cache.latest.files.RELEASES, {
 		headers: {
